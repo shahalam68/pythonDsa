54b8976ae771b3a08b816a4e1e4e9b5fa3d9a470
shshalam68 <a.k.mshahalam68@gmail.com>
2025-07-10 23:43:52 +0600

    test

---
+++ InputOutput.py
@@ -0,0 +1,13 @@
+# Output 
+print()
+
+name = "Soykot"
+age = 29
+# Raw String
+print("hello my name is", name, "my age is", age);
+#  Formated String
+# if i add f we can write the formating string 
+print(f"Hello my name is {name} and my age is {age}")
+
+age =  int(input("Heloo what is your age "))
+print(age);
---
+++ operators.py
@@ -0,0 +1,9 @@
+# artematic opertator 
+a = 12 
+b = 20 
+
+print(a + b)
+
+print(b -a)
+print(a*b)
+
