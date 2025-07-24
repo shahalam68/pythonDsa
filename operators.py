8ed506bf663a7ce99e44dc8c5e882eee02a3987c
shshalam68 <a.k.mshahalam68@gmail.com>
2025-07-24 07:32:04 +0600

    oparetors

--- operators.py
+++ operators.py
@@ -1,9 +1,23 @@
 # artematic opertator 
-a = 12 
-b = 20 
+# a = 12 
+# b = 20 
 
-print(a + b)
+# print(a + b)
 
-print(b -a)
-print(a*b)
+# print(b -a)
+# print(a * b)
+
+
+# compound asssignment operation 
+
+# a = 20
+
+# # a = a + 20 
+# # or
+# a += 20
+# # a = a + 40
+# a += 60
+# print (a )
+
+# Comparission operation 
 
